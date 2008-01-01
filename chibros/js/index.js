a7f22ef88c10caca96a2be1dcc395062159eedee
chibros <chibroskingdom1@gmail.com>
2008-01-01 15:24:15 +0100

    improved my project

--- chibros/js/index.js
+++ chibros/js/index.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){    
+$(document).ready(function(){  
     $.ajax({
         type:"post",
         url:"http://localhost/api_demo/api/manage_post.php",    
@@ -8,6 +8,7 @@ $(document).ready(function(){
                 post_id: 'post id/sn',
              },
         success:function(response){ 
+        
              $loop_array="";       
              response.posts.forEach(function(post){                                      
                     $loop_array += `<div class="content"> 
@@ -19,10 +20,10 @@ $(document).ready(function(){
                                 </header>          
                                    <img src="images/construct.jpg" class="pimg"> 
                                    <p>${post.body}</p>                               
-                                   <p style="color:red;paddind:10px; margin-top:10px;"><span style="margin:auto 10px;">Author:</span>${post.author.firstname} ${post.author.lastname}</p>
-                                   <p style="color:red;paddind:10px;"><span style="margin:auto 10px;">Authors_ID:</span>${post.author.id}</p>
-                                   <p style="color:red;paddind:10px;"><span style="margin:auto 10px;">Date of post:</span>${post.created_at}</p>      
-                             </div> `                                                                           
+                                   <p><span>Author:</span>${post.author.firstname} ${post.author.lastname}</p>
+                                   <p><span>Authors_ID:</span>${post.author.id}</p>
+                                   <p><span>Date of post:</span>${post.created_at}</p>      
+                             </div>`                                                                           
              });                 
             $result=$("#container").html($loop_array);                                                    
         },
