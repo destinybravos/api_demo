9f4a7cb6418bff3a0007bf1f964d2f9556fc6a96
chibros <chibroskingdom1@gmail.com>
2020-10-02 14:10:05 +0100

    worked on post,view,edit,delete yet to be complete

--- chibros/js/post.js
+++ chibros/js/post.js
@@ -7,31 +7,45 @@
         },
         dataType:"json",
         success:function(response){
-            Let session_memory=$("memory_sesssion").text(response);
+           $("#memory_session").val(response.access_token);
         },
         error:function(xhr, status, msg){
                 console.log(msg);                    
         } 
         });
-    $("#post_form").submit(function(e){
-                            var token=response;       
-                            $title=$("title").val();
-                            $body=$("textarea").val();     
-                    $.ajax({
-                                type:"post",
-                                url:"http://localhost/api_demo/api/manage_post.php",    
-                                dataType:"json",
-                                data:{    
-                                        action: 'save_post', 
-                                        title: $title, 
-                                        body: $body, 
-                                        token: token
-                                    },
-                                success:function(response){
-                                    alert("insertion successful");                                  
-                                },
-                                error:function(xhr, status, msg){
-                                    console.log(msg);                    
-                                } 
-                            });
+    $("#post_form").submit(function(e){ 
+                    e.preventDefault();                   
+                            $token=$("#memory_session").val();      
+                            $title=$("#title").val();
+                            $body=$("#textarea").val();     console.log($token);
+                            savepost();
                     });
+               
+ function savepost(){
+                    $.ajax({
+                            type:"post",
+                            url:"http://localhost/api_demo/api/manage_post.php",    
+                            dataType:"json",
+                            data:{    
+                                    action: 'save_post', 
+                                    title: $title, 
+                                    body:  $body, 
+                                    token: $token
+                                },
+                            success:function(response){
+                                alert(response.message);
+                                $title=$("#title").val("");
+                                $body=$("#textarea").val("");                                 
+                            },
+                            error:function(xhr, status, msg){
+                                console.log(msg);                    
+                            } 
+                        });
+ }
+ $("#btn_view_post").click(function(){     
+      window.location.href="http://localhost/api_demo/chibros/index.html";   
+ });
+ $("#btn_edit_post").click(function(){     
+    window.location.href="http://localhost/api_demo/chibros/editpost.html";
+ });    
+    
