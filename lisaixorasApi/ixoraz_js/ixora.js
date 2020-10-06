f23740795daf73c73573c0434b5b73d9821a23b3
LISAIXORA <okalaumahappiness@gmail.com>
2020-10-05 20:58:24 -0600

    loading

--- lisaixorasApi/ixoraz_js/ixora.js
+++ lisaixorasApi/ixoraz_js/ixora.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
-    $("#advert0").css("display", "none");
+    $("#advert0").css("display", "none").slideUp("10000").slideDown("1000");
     $("#advert1").fadeOut("slow");
     $("#advert0").fadeIn("slow");
+    $("#advert3").slideUp("10000").slideDown("1000");
 });
 
 $(document).ready(function(){
@@ -33,6 +34,76 @@ $(document).ready(function(){
 
     });
 
+    $("#btnlgn").click(function(){
     if("#cpass"==="#regpass"){
-        location.
-    }
+     console.log("submit?");}
+        });
+
+
+        $('#regform').on('submit',function (x) {
+            x.preventDefault();
+
+            $fname = $('#fname').val();
+            $lname = $('#lname').val();
+            $email = $('#regmail').val();
+            $phone = $('#phone').val();
+            $pass = $('#regpass').val();
+            $cpass = $('#cpass').val();
+        
+            if($pass === $cpass){
+                let datasend = {
+                    firstname: $fname,
+                    lastname: $lname,
+                    email: $email,
+                    phone: $phone,
+                    password: $pass
+                }
+            
+            registerindividual(datasend);
+        }else{
+            alert('Password Mismatched');
+        }
+        });
+
+function registerindividual(userdata){
+
+    $.ajax({
+        type : 'post',
+        url : 'http://localhost/api_demo/api/register.php',
+        data: userdata,
+        dataType: 'json',
+        success: function (response) {
+            console.log(response);
+            if(response.status == 'success'){
+                alert(response.message);
+                window.location.href = 'http://localhost/api_demo/lisaixorasApi/ixoravalidate.html';
+            }else{
+                alert(response.message);
+            }
+        },
+        error: function(xhr, status, msg){
+            console.log(msg);
+        }
+    })
+
+}
+
+
+// to enter dashboard through login page//
+$("#validateform").on("submit",function(e){     
+    e.preventDefault();
+
+    $userid=$("#usern").val();        
+    $pass=$("#valipass").val();
+    $.ajax({
+        type:"post",
+        url:"http://localhost/api_demo/api/login.php",
+        data:{
+        user_id:$userid,        
+        password:$pass
+        },
+        dataType:'json'
+       
+    });
+    console.log(e);
+});
